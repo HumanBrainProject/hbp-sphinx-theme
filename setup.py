fe60cb7698ad6611eed5fd7d67421a77166fb004
Olivier Amblet <olivier.amblet@epfl.ch>
2015-07-10 14:55:21 +0200

    fixed README.rst path when installing from sdist
    Change-Id: I4afbc172b4c93c69a6e8741df90d36d2a7ec5543

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@
 from setuptools import setup
 from collaboratory_sphinx_theme import __version__
 
+import os
+
+directory = os.path.abspath(os.path.dirname(__file__))
+
 setup(
     name='collaboratory_sphinx_theme',
     version=__version__,
@@ -12,7 +16,7 @@ setup(
     author='Olivier Amblet',
     author_email='olivier.amblet@epfl.ch',
     description='Collaboratory theme for Sphinx, 2015 version.',
-    long_description=open('README.rst').read(),
+    long_description=open(os.path.join(directory, 'README.rst')).read(),
     zip_safe=False,
     packages=['collaboratory_sphinx_theme'],
     package_data={'collaboratory_sphinx_theme': [
